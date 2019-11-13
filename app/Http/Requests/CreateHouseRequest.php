5c0e703defc3b7e859a83ea4b3f6f981ad07148b
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-11-13 14:34:03 +0700

    validate form create house

---
+++ app/Http/Requests/CreateHouseRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateHouseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'name'=>'required',
+            'address'=>'required|min:5',
+            'totalBedRoom'=>'required',
+            'totalBathRoom'=>'required',
+            'description'=>'required',
+            'price'=>'required',
+            'status'=>'required',
+            'image'=>'mimes:jpeg,bmp,png',
+            'category_id'=>'required',
+            'user_id'=>'required'
+        ];
+    }
+}
