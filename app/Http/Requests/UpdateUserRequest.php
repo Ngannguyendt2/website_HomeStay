232ed5ef638e487251275f2b808cb863a97b0fa2
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-11-15 17:04:28 +0700

    update validate

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -30,7 +30,7 @@ class UpdateUserRequest extends FormRequest
         return [
             //
             'name' => 'required|min:5|max:50',
-            'phone' => ['required', Rule::unique('users')->ignore(Auth::user()->id),],
+            'phone' => ['required', 'regex:/^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/', Rule::unique('users')->ignore(Auth::user()->id),],
             'address' => 'required|min:5',
             'image' => 'mimes:jpeg,bmp,png'
         ];
@@ -43,6 +43,7 @@ class UpdateUserRequest extends FormRequest
             'name.min' => 'Tên phải lớn hơn 5 ký tự',
             'name.max' => 'Tên không quá 50 ký tự',
             'phone.required' => 'Vui lòng không để trống số điện thoại',
+            'phone.regex' => 'số điện thoại không đúng',
             'address.required' => 'Vui lòng nhập địa chỉ',
 
         ];
