0ca43a55c8952657583bde24d92d890242841d1b
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-11-18 13:34:57 +0700

    upload multiple image

--- app/House.php
+++ app/House.php
@@ -8,10 +8,23 @@ class House extends Model
 {
     protected $fillable = ['approved_at'];
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo('App\User');
     }
-    public function category(){
+
+    public function category()
+    {
         return $this->belongsTo('App\Category');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Order');
+    }
+
+    public function customers()
+    {
+        return $this->belongsToMany('App\Customer', 'orders');
     }
 }
