8562a0ec22fd9fe418f490a0f8b044e3f9279bb7
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-11-22 13:39:51 +0700

    validate check out

--- app/Http/Requests/CreateFormOrder.php
+++ app/Http/Requests/CreateFormOrder.php
@@ -26,8 +26,8 @@ class CreateFormOrder extends FormRequest
         return [
             //
             'phone' => 'required', 'regex:/^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/',
-            'checkin' => 'required',
-            'checkout' => 'required'
+            'checkin' => 'required|after:today',
+            'checkout' => 'required|after:checkin'
         ];
     }
 }
